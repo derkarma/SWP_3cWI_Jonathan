cda18f714395742d62bce42e800fd863a50ffca3
jonathan <jonathan.stampfer@gmail.com>
2021-12-15 09:06:23 +0100

    palindrom

--- src/at/jonathan/stringUebungen/StringHelper.java
+++ src/at/jonathan/stringUebungen/StringHelper.java
@@ -3,7 +3,29 @@ package at.jonathan.stringUebungen;
 public class StringHelper {
     public static void main(String[] args) {
 
+        //Example for Palindrom: mom, dad, rotor, race car
 
+        String originalText = "race car";
+        originalText = originalText.replace(" ", "");
 
+        String reverseText = "";
+        for (int i = originalText.length() - 1; i >= 0; i--) {
+            reverseText += originalText.charAt(i);
+            System.out.println(reverseText);
+        }
+
+        boolean palindrome = true;
+        for (int i = 0; i < originalText.length(); i++) {
+            if (originalText.charAt(i) != reverseText.charAt(i)) {
+                palindrome = false;
+            }
+        }
+        if (palindrome) {
+            System.out.println("Es ist ein Palindrom!");
+        } else {
+            System.out.println("Es ist kein Palindrom!");
+        }
     }
+
+
 }
