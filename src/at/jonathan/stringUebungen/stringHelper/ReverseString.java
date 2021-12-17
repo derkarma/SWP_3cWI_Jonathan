299c5e2c24fe45759b2346e262cebc2bc8e82d62
jonathan <jonathan.stampfer@gmail.com>
2021-12-17 11:20:37 +0100

    next up

---
+++ src/at/jonathan/stringUebungen/stringHelper/ReverseString.java
@@ -0,0 +1,15 @@
+package at.jonathan.stringUebungen.stringHelper;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String originalText = "Jobban ist cool";
+        String reverseText = "";
+
+        for (int i = originalText.length() - 1; i >= 0; i--) {
+            reverseText += originalText.charAt(i);
+            System.out.println(reverseText);
+        }
+
+    }
+}
