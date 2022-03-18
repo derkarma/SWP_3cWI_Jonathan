1961e964950e59a7b145e8da87a0def08f6ab51e
jonathan <jonathan.stampfer@gmail.com>
2022-03-18 12:18:29 +0100

    still working on remote control

--- src/at/jonathan/objectOrientation/RemoteControl/Remote.java
+++ src/at/jonathan/objectOrientation/RemoteControl/Remote.java
@@ -17,14 +17,21 @@ public class Remote {
 
    public void turnOn(){
        int chargingStatus1 = this.batteries.get(0).getChargingStatus();
-       int chargringStatus2 = this.batteries.get(1).getChargingStatus();
+       int chargingStatus2 = this.batteries.get(1).getChargingStatus();
 
+       this.batteries.get(0).setChargingStatus(chargingStatus1-(chargingStatus1 * 5/100));
+       this.batteries.get(1).setChargingStatus(chargingStatus2-(chargingStatus2 * 5/100));
+
+       int newChargingStatus1 = this.batteries.get(0).getChargingStatus();
+       int newChargingStatus2 = this.batteries.get(1).getChargingStatus();
    }
 
    public void turnOff(){
        System.out.println("Kein Verbraucher angeschlossen");
    }
 }
+
+
 
 
 
